2492db879b23cb4d6e19137470c2e2720788f7f5
Jinyoung Jang <redpwon@gmail.com>
2024-01-02 11:32:29 +0900

    Fix : heading 태그가 없을 경우 페이지 안열리는 오류 수정

--- components/mdx/Toc.tsx
+++ components/mdx/Toc.tsx
@@ -1,25 +1,25 @@
 'use client';
 
-import {useEffect, useState} from 'react';
-import {getIntersectionObserver} from "@/util/Toc";
+import { useEffect, useState } from 'react';
+import { getIntersectionObserver } from "@/util/Toc";
 
 export default function TableOfContents() {
     const [headingEls, setHeadingEls] = useState<Element[]>([]);
-
-    // 현재 위치한 목차 상태
     const [activeId, setActiveId] = useState<string>('');
-    
+
     useEffect(() => {
         const article = document.querySelector('article');
         if (article) {
             const headingElements = Array.from(article.querySelectorAll('h2, h3'));
-            setHeadingEls(headingElements);
-            //처음에는 첫번째 목차로 id 업데이트
-            setActiveId(headingElements[0].id);
-            const observer = getIntersectionObserver(setActiveId);
-            headingElements.map((element) => {
-                observer.observe(element);
-            });
+
+            if (headingElements.length > 0) {
+                setHeadingEls(headingElements);
+                setActiveId(headingElements[0].id);
+                const observer = getIntersectionObserver(setActiveId);
+                headingElements.map((element) => {
+                    observer.observe(element);
+                });
+            }
         }
     }, []);
 
@@ -30,10 +30,10 @@ export default function TableOfContents() {
     return (
         <ul className="mt-[100px] ml-10">
             {headingEls.map((headingEl: Element, index: number) => (
-                <li className={`list-none text-gray-400 ${headingEl.id === activeId ? 'text-black' : ''}`} key={index}
+                <li className={`list-none text-gray-400 ${headingEl?.id === activeId ? 'text-black' : ''}`} key={index}
                     style={{ marginLeft: `${getHeadingLevel(headingEl.tagName) - 2}em` }}>
-                    <a href={`#${headingEl.id}`} className={` ${headingEl.id === activeId ? 'text-black' : ''}`}>
-                        {headingEl.id}
+                    <a href={`#${headingEl?.id}`} className={` ${headingEl?.id === activeId ? 'text-black' : ''}`}>
+                        {headingEl?.id}
                     </a>
                 </li>
             ))}
