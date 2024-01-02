843876332c9b162b3aaa0c7d976b0711fe66c5ca
Jinyoung Jang <redpwon@gmail.com>
2024-01-02 14:30:04 +0900

    heading 태그가 없는 경우 TOC 에러 수정

--- components/mdx/Toc.tsx
+++ components/mdx/Toc.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { getIntersectionObserver } from "@/util/Toc";
+import React, { useEffect, useState } from 'react';
 
 export default function TableOfContents() {
     const [headingEls, setHeadingEls] = useState<Element[]>([]);
@@ -11,32 +10,58 @@ export default function TableOfContents() {
         const article = document.querySelector('article');
         if (article) {
             const headingElements = Array.from(article.querySelectorAll('h2, h3'));
+            setHeadingEls(headingElements);
 
+            // 처음에는 첫 번째 목차로 id 업데이트
             if (headingElements.length > 0) {
-                setHeadingEls(headingElements);
                 setActiveId(headingElements[0].id);
-                const observer = getIntersectionObserver(setActiveId);
-                headingElements.map((element) => {
-                    observer.observe(element);
-                });
             }
+
+            const observer = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.isIntersecting) {
+                            setActiveId(entry.target.id);
+                        }
+                    });
+                },
+                {
+                    rootMargin: '0px 0px -90% 0px', // 목차가 화면의 일부만 보여도 활성화될 수 있도록 설정
+                }
+            );
+
+            headingElements.map((element) => {
+                if (element) {
+                    observer.observe(element);
+                }
+            });
         }
     }, []);
-
-    const getHeadingLevel = (tagName: string): number => {
-        return parseInt(tagName.slice(1), 10);
-    };
 
     return (
         <ul className="mt-[100px] ml-10">
             {headingEls.map((headingEl: Element, index: number) => (
-                <li className={`list-none text-gray-400 ${headingEl?.id === activeId ? 'text-black' : ''}`} key={index}
-                    style={{ marginLeft: `${getHeadingLevel(headingEl.tagName) - 2}em` }}>
-                    <a href={`#${headingEl?.id}`} className={` ${headingEl?.id === activeId ? 'text-black' : ''}`}>
-                        {headingEl?.id}
+                <li
+                    className={`list-none text-gray-400 ${
+                        headingEl.id === activeId ? 'text-black' : ''
+                    }`}
+                    key={index}
+                    style={{ marginLeft: `${getHeadingLevel(headingEl.tagName) - 2}em` }}
+                >
+                    <a
+                        href={`#${headingEl.id}`}
+                        className={` ${
+                            headingEl.id === activeId ? 'text-black' : ''
+                        }`}
+                    >
+                        {headingEl.id}
                     </a>
                 </li>
             ))}
         </ul>
     );
+}
+
+function getHeadingLevel(tagName: string): number {
+    return parseInt(tagName.slice(1), 10);
 }
