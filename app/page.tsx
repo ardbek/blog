04300a083e5f913a59815fb627c905747e8d230a
Jinyoung Jang <redpwon@gmail.com>
2024-01-03 10:00:11 +0900

    Feat: 게시글 날짜 기준 내림차순 정렬 추가

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ export default function Home() {
         const isDirectory = fs.statSync(itemPath).isDirectory();
 
         if (isDirectory) {
-            // 디렉토리인 경우, 해당 디렉토리 내의 모든 파일을 처리
+
             const files = fs.readdirSync(itemPath).filter(file => fs.statSync(path.join(itemPath, file)).isFile());
             files.forEach(filename => {
                 const filePath = path.join(itemPath, filename);
                 const fileContent = fs.readFileSync(filePath, 'utf-8');
-                const { data: frontMatter } = matter(fileContent);
+                const {data: frontMatter} = matter(fileContent);
 
                 posts.push({
                     year: item,
@@ -29,9 +29,8 @@ export default function Home() {
                 });
             });
         } else {
-            // 파일인 경우, 바로 목록에 추가
             const fileContent = fs.readFileSync(itemPath, 'utf-8');
-            const { data: frontMatter } = matter(fileContent);
+            const {data: frontMatter} = matter(fileContent);
 
             posts.push({
                 meta: frontMatter,
@@ -40,23 +39,29 @@ export default function Home() {
         }
     });
 
+    posts.sort((a, b) => {
+        return new Date(b.meta.date).getTime() - new Date(a.meta.date).getTime();
+    });
+
     return (
         <main className="p-3 mx-auto sm:w-10/12 md:w-10/12 lg:w-10/12 xl:w-4/12">
             <div>
-                {posts.map(post => (
-                    <Link href={post.year ? `/posts/${post.year}/${post.slug}` : `/posts/${post.slug}`} passHref key={post.slug}>
-                        <div className='group border rounded border-slate-300 mb-1 p-2 transition-all hover:border-slate-400'>
-                            <div className="flex justify-between">
-                                <span className="transition-all group-hover:underline">{post.meta.title}</span>
-                                <span className="text-slate-400 text-xs">{post.meta.date}</span>
+                {
+                    posts.map(post => (
+                        <Link href={post.year ? `/posts/${post.year}/${post.slug}` : `/posts/${post.slug}`} passHref
+                              key={post.slug}>
+                            <div
+                                className='group border rounded border-slate-300 mb-1 p-2 transition-all hover:border-slate-400'>
+                                <div className="flex justify-between">
+                                    <span className="transition-all group-hover:underline">{post.meta.title}</span>
+                                    <span className="text-slate-400 text-xs">{post.meta.date}</span>
+                                </div>
+                                <div className="">
+                                    <p className="text-xs text-slate-500">{post.meta.description}</p>
+                                </div>
                             </div>
-                            <div className="">
-                                <p className="text-xs text-slate-500">{post.meta.description}</p>
-                            </div>
-                        </div>
-                    </Link>
-                ))}
-
+                        </Link>
+                    ))}
             </div>
         </main>
     );
