38cdd7fbb5c4e5930cb1f58c7ab70db1df2d3e35
Jinyoung Jang <redpwon@gmail.com>
2024-03-22 17:05:48 +0900

    feat: 이미지 최적화

--- components/mdx/MDXComponent.tsx
+++ components/mdx/MDXComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ExternalLinkIcon from './ExternalLinkIcon';
 import {CopyButton} from "@/components/mdx/CopyButton";
+import Image from 'next/image';
 
 interface MDXComponentProps {
     children: React.ReactNode;
@@ -18,6 +19,11 @@ function reactNodeToString(node: React.ReactNode): string {
 }
 
 const MDXComponent: React.FC<MDXComponentProps> = ({children}) => {
+
+    const renderImage = (props:any) =>{
+        return <Image {...props} layout="responsive" width={100} height={50}/>; // 실제 이 크기로 랜더링 되는것은 아님.
+    }
+
     const renderLink = (props: any) => {
         const {href} = props;
         const isExternal = href.startsWith('http') || href.startsWith('//');
@@ -53,7 +59,8 @@ const MDXComponent: React.FC<MDXComponentProps> = ({children}) => {
                 React.isValidElement(child) ? React.cloneElement(child as React.ReactElement<any>, {
                     components: {
                         a: renderLink,
-                        pre: renderPre
+                        pre: renderPre,
+                        img: renderImage
                     }
                 }) : child
             )}
